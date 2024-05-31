9d0711f141ae793a3155af8fe11b98235cd84d5b
Melina Moraiti <melinamoraith@gmail.com>
2024-05-31 18:31:34 +0300

    Make smoother rotation and rotate camera

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Transform cam;
     public float speed = 6f;
-
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     // Update is called once per frame
     void Update()
     {
@@ -15,10 +18,12 @@ public class ThirdPersonMovement : MonoBehaviour
         Vector3 direction = new Vector3 (horizontal, 0, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            controller.Move(direction * speed * Time.deltaTime);
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward; 
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
         }
 
 
