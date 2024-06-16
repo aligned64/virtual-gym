73bc560d9bef528c5c0d6ae9c7a206b825ba6e12
marioskam3 <85133854+marioskam3@users.noreply.github.com>
2024-06-16 12:37:24 +0300

    not showing in inventory

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,11 +1,19 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
     public List<Item> items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
 
     public void Awake()
     {
@@ -20,5 +28,24 @@ public class InventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshPro>();
+            var itemIcon = obj.transform.Find("itemIcon").GetComponent<Image>();
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
     }
 }
